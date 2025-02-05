e1754326c934e5875a6401bac04a2a49d13b6651
matkuz162 <kuzonmateusz60@gmail.com>
2025-02-05 16:24:44 +0000

    admin web.php route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SensorDataController::class, 'index'])->name('sensorData.index');
 Route::get('/', [SensorController::class, 'index'])->name('sensors.index');
+Route::get('/', [AdminController::class, 'index'])->name('admin.index');
 
 // Route::get('/dashboard', function () {
 //     return view('');
