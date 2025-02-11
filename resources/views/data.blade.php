5a908406d20a23ea911618e829b055d01c5a430c
ScottForster400 <scottforster400@gmail.com>
2025-02-11 10:38:03 +0000

    Added date time picker and info cards

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -10,7 +10,7 @@
             <form action="" class = "w-4/5">
                 <x-search-bar-gps placeholder="Search for a Sensor..."></x-search-bar-gps>
             </form>
-            <x-card>
+            <x-card class="mb-2">
                 <div id="card-top" class="flex flex-row h-20">
                     <div id="card-top-left" class="flex flex-col basis-3/4 justify-between">
                         <h2>Sensor Location</h2>
@@ -43,9 +43,34 @@
                 </div>
             </x-card>
 
-            <div >
+            <div class="flex flex-row justify-evenly w-10/12">
+                <x-card class="basis-1/2 w-full mr-2 mt-2 flex flex-col items-center text-center justify-between px-2 hover:bg-gray-100 hover:shadow-xl transition-all">
+                    <div class="h-8 w-8 flex justify-center items-center">
+                        <img src="{{URL::asset('imgs/temp.svg')}}"" alt="dissolved_oxygen" class="w-full h-full">
+                    </div>
+                    <h3>12°c</h3>
+                    <p class="text-gray-500 text-xs">Temperature</p>
+                </x-card>
 
+                <x-card class="basis-1/2 w-full ml-2 mt-2 flex flex-col items-center text-center justify-between px-2 hover:bg-gray-100 hover:shadow-xl transition-all">
+                    <div class="h-9 w-9 flex justify-center items-center ">
+                        <img src="{{URL::asset('imgs/DO.svg')}}"" alt="dissolved_oxygen" class="w-full h-full">
+                    </div>
+                    <h3>12 mg/l</h3>
+                    <p class="text-gray-500 text-xs">Dissolved Oxygen</p>
+                </x-card>
             </div>
+            <x-card class="px-2">
+                <h2 class="m">
+                    Sensor Data
+                </h2>
+                <form action="" class="w-full flex justify-center">
+                    <x-date-time-picker></x-date-time-picker>
+                </form>
+                <div id="graph" class="h-32 flex items-center justify-center border-gray-400 border-solid border-2 my-2">
+                    <p>Graph placeholder</p>
+                </div>
+            </x-card>
         </div>
     </div>
 
