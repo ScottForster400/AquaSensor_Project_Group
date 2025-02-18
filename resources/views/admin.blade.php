24567623be961a5f75c2b1031da72a3a82e9fc89
Naz2833 <149158744+Naz2833@users.noreply.github.com>
2025-02-18 11:35:54 +0000

    aaaaaaaaaaa

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,6 +7,7 @@
     
     <p>Sensors</p>
 
+
     <x-modal-toggle data-modal-target="edit" data-modal-toggle="edit">Create Sensor</x-modal-toggle>
             <!-- Modal to create Sensor -->
             <x-modal id="edit" class="bg-gray-500 bg-opacity-75 h-full">
@@ -14,10 +15,51 @@
                 <x-modal-body>
                     <form>
                         <div class="grid gap-6 mb-6 md:grid-cols-2">
-                            <button type="Create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
                         </div>
                     </form>
                 </x-modal-body>
             </x-modal>
+
+            <x-modal-toggle data-modal-target="edit" data-modal-toggle="edit">Delete Sensor</x-modal-toggle>
+            <x-modal id="edit" class="bg-gray-500 bg-opacity-75 h-full">
+                <x-modal-header data-modal-hide="edit">Delete Sensor</x-modal-header>
+                <x-modal-body>
+                    <form>
+                        <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete</button>
+                        </div>
+                    </form>
+                </x-modal-body>
+            </x-modal>
+
+            <p>Users</p>
+
+            <x-modal-toggle data-modal-target="edit" data-modal-toggle="edit">Create User </x-modal-toggle>
+            <x-modal id="edit" class="bg-gray-500 bg-opacity-75 h-full">
+                <x-modal-header data-modal-hide="edit">Create User</x-modal-header>
+                <x-modal-body>
+                    <form>
+                        <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create User</button>
+                        </div>
+                    </form>
+                </x-modal-body>
+            </x-modal>
+            
+            <x-modal-toggle data-modal-target="edit" data-modal-toggle="edit">Delete User</x-modal-toggle>
+            <x-modal id="edit" class="bg-gray-500 bg-opacity-75 h-full">
+                <x-modal-header data-modal-hide="edit">Delete User</x-modal-header>
+                <x-modal-body>
+                    <form>
+                        <div class="grid gap-6 mb-6 md:grid-cols-2">
+                            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete User</button>
+                        </div>
+                    </form>
+                </x-modal-body>
+            </x-modal>
+
+
+
 
 </x-app-layout>
