59ab98c5fcb2e4cbe669a20e4f611a11504ff716
Zephyri0s <149779192+Zephyri0s@users.noreply.github.com>
2025-02-18 13:50:08 +0000

    JSgeolocation

---
+++ public/js/userLocation.js
@@ -0,0 +1,8 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        console.log("latitude: " + latitude + " longitude: " + longitude);
+    })
+}
