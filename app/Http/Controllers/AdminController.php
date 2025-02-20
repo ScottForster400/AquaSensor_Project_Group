09cb50385117b442756632c72fc3aab20ab2d20f
Sovietsupporter69 (laptop) <84580170+Sovietsupporter69@users.noreply.github.com>
2025-02-20 17:05:33 +0000

    Re-implemented the create sensor functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Sensor;
+use App\Models\Sensor_Data;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -21,51 +23,72 @@ class AdminController extends Controller
         return view('admin',compact('allsensors','allusers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function createUser(Request $request)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
+        $request ->validate([
+            'name'=> 'required|max:255|string',
+            'email'=> 'required|max:255|string|lowercase|email|unique:App\Models\user,email',
+            'password'=> 'required|max:255',
+            'admin'=> 'boolean',
+            'companyname'=> 'required|max:255|string'
+        ]);
+        $newUser = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'admin' => $request->admin,
+            'company_name' => $request->companyname
+        ]);
+        $newUser ->save();
+        return to_route('admin.index')->with('success', "New user made successfully");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroyUser(User $user)
     {
-        //
+        if (Auth::user()->admin != 1) {
+            abort(403);
+        }
+        
+        $ownedSensors = Sensor::where('id', $user->id);
+        foreach ($ownedSensors as $sensor) {
+            Sensor_Data::where('sensor_id', $sensor->id)->delete();
+            $sensor->delete();
+        }
+        $user->delete();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function createSensor(Request $request)
+    {
+        $request->validate([
+            'sensor_id'=> 'required|string|max:255|unique:App\Models\Sensor,sensor_id',
+            'activation_key'=> 'string|max:16'
+        ]);
+        $sensor = new Sensor([
+            'sensor_id' => $request->sensor_id,
+            'opensource' => 0,
+            'activated' => 0,
+            'activation_key' => $request->activation_key
+        ]);
+        $sensor->save();
+        return to_route('admin.index')->with('success', "New sensor made successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroySensor(Sensor $sensor)
+    {
+        if (Auth::user()->admin != 1) {
+            abort(403);
+        }
+        
+        $sensor->delete();
+        Sensor_Data::where('sensor_id', $sensor->id)->delete();
     }
 }
