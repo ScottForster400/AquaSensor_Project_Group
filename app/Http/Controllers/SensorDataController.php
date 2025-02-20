eb7e91918664bcc2d81cece2084993c48d191f0a
Sovietsupporter69 (laptop) <84580170+Sovietsupporter69@users.noreply.github.com>
2025-02-20 14:28:33 +0000

    Added the filp card average calculations to the data page
    Also moved the api calls to a function to make it super easy to use

--- app/Http/Controllers/SensorDataController.php
+++ app/Http/Controllers/SensorDataController.php
@@ -14,56 +14,20 @@ class SensorDataController extends Controller
      */
     public function index()
     {
-        $response = null;
+        
         $sensorCount = Count(Sensor::where('opensource', 1)->where('activated', 1)->get());
-        $curl = curl_init();
-        $apiURL = 'https://api.aquasensor.co.uk/aq.php?op=readings&username=shu&token=aebbf6305f9fce1d5591ee05a3448eff&sensorid=';
 
         if ($sensorCount > 0) {
-            //gets all sensor data from api
-            // Mats special code
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             if (array_key_exists('sensor_id', $_REQUEST)) {
-                $apiURL .= $_REQUEST['sensor_id'];
+                $activeSensor = $_REQUEST['sensor_id'];
+                $data = $this->GetAndFormatCurl($activeSensor);
             } else {
                 $allSensors = Sensor::where('opensource', 1)->where('activated', 1)->get();
                 $randomSensors = $allSensors[random_int(0, count($allSensors) -1)];
-                $apiURL .= "sensor022";
-                //$apiURL .= $randomSensors->sensor_id;
+                $activeSensor = "sensor022";
+                $data = $this->GetAndFormatCurl($activeSensor);
             }
-
-            //gets api data
-            curl_setopt_array($curl, array(
-                    CURLOPT_URL => $apiURL,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'GET',
-            ));
-
-            $response = curl_exec($curl);
-
-            curl_close($curl);
-
-            //gets rid of the \n on the new lines to allow iteration
-            $response = rtrim($response, "\n");
-            $rows = explode("\n", $response);
-
-            //cleans up any extra newlines if the trim didnt work.
-            //https://www.php.net/manual/en/function.array-filter.php
-            $rows = array_filter($rows, function ($row) {
-                return !empty(trim($row));
-            });
-            $rows = array_values($rows);
-
-            //converts to array.
-            $data = array_map('str_getcsv', $rows);
-
-            //removes headers as first array entry
-            array_shift($data);
+            
             $temp=2;
             $do=3;
             $date=0;
@@ -83,12 +47,16 @@ class SensorDataController extends Controller
                 $dataAverager = array_splice($mobileData, 0, $mobileAverageCount);
 
                 for ( $j = 0; $j < count($dataAverager); $j++) {
-                    $averageTempData += $dataAverager[$j][$temp];
-                    $averageDoData += $dataAverager[$j][$do];
+                    if ($dataAverager[$j][$temp] >= -30 && $dataAverager[$j][$temp] <= 120 && $dataAverager[$j][$do] >= 0 && $dataAverager[$j][$do] <= 65) {
+                        $averageTempData += $dataAverager[$j][$temp];
+                        $averageDoData += $dataAverager[$j][$do];
+                    } else {
+                        array_splice($dataAverager, $j, 1);
+                    }
                 }
                 $mobileAveragedData[$temp]->push(number_format($averageTempData/count($dataAverager), 3, '.', ''));
                 $mobileAveragedData[$do]->push(number_format($averageDoData/count($dataAverager), 3, '.', ''));
-                $mobileAveragedData[$date]->push($dataAverager[0][$date] . " - " . $dataAverager[$mobileDataLength][$date]);
+                $mobileAveragedData[$date]->push($dataAverager[0][$date] . " - " . $dataAverager[count($dataAverager)-1][$date]);
                 $mobileAveragedData[$time]->push($dataAverager[0][$time]);
             }
 
@@ -99,20 +67,39 @@ class SensorDataController extends Controller
 
                 $dataAverager = array_splice($data, 0, $averageCount);
                 for ( $j = 0; $j < count($dataAverager); $j++) {
-                    $averageTempData += $dataAverager[$j][$temp];
-                    $averageDoData += $dataAverager[$j][$do];
+                    if ($dataAverager[$j][$temp] >= -30 && $dataAverager[$j][$temp] <= 120 && $dataAverager[$j][$do] >= 0 && $dataAverager[$j][$do] <= 65) {
+                        $averageTempData += $dataAverager[$j][$temp];
+                        $averageDoData += $dataAverager[$j][$do];
+                    } else {
+                        array_splice($dataAverager, $j, 1);
+                    }
                 }
-                $averagedData[$temp]->push(number_format($averageTempData/count($dataAverager), 3, '.', ''));
-                $averagedData[$do]->push(number_format($averageDoData/count($dataAverager), 3, '.', ''));
-                $averagedData[$date]->push($dataAverager[0][$date] . " - " . $dataAverager[$dataLength][$date]);
+                $averagedData[$temp]->push(number_format($averageTempData/count($dataAverager), 3));
+                $averagedData[$do]->push(number_format($averageDoData/count($dataAverager), 3));
+                $averagedData[$date]->push($dataAverager[0][$date] . " - " . $dataAverager[count($dataAverager)-1][$date]);
                 $averagedData[$time]->push($dataAverager[0][$time]);
             }
             $currentSensorData = Sensor_Data::where('sensor_id',$randomSensors->sensor_id);
-            // dd($currentSensorData);
 
-            //dd($mobileAveragedData);
+            $flipCardData = [
+                $this->GetAndFormatCurl($activeSensor . "&fromdate=" . date('d-m-y') . "&todate=" . date('d-m-y')),
+                $this->GetAndFormatCurl($activeSensor . "&fromdate=" . date('d-m-y', strtotime('-'.(string)(date('w')-1).' days')) . "&todate=" . date('d-m-y')),
+                $this->GetAndFormatCurl($activeSensor . "&fromdate=" . date('d-m-y', strtotime('-'.(string)(date('j')-1).' days')) . "&todate=" . date('d-m-y'))
+            ];
+            $averagedFlipData = [[0, 0, 0], [0, 0, 0]];
+            for ($i= 0; $i<count($flipCardData); $i++) {
+                $tempAverager = 0;
+                $doAverager = 0;
+                for ($j= 0; $j<count($flipCardData); $j++) {
+                    $tempAverager += $flipCardData[$i][$j][$temp];
+                    $doAverager += $flipCardData[$i][$j][$do];
+                }
+                $averagedFlipData[0][$i] = number_format($tempAverager/count($flipCardData[$i][$j]), 3);
+                $averagedFlipData[1][$i] = number_format($doAverager/count($flipCardData[$i][$j]), 3);
+            }
+            dd($averagedFlipData);
 
-            return view('data')->with('mobileAveragedData',$mobileAveragedData)->with('desktopAveragedData',$averagedData);
+            return view('data')->with('mobileAveragedData',$mobileAveragedData)->with('desktopAveragedData',$averagedData)->with('flipCardData', $averagedFlipData);
         }
         else{
             return view('data');
@@ -167,5 +154,47 @@ class SensorDataController extends Controller
     public function destroy(Sensor_Data $sensor_Data)
     {
         //
+    }
+
+    private function GetAndFormatCurl($search) {
+        $apiURL = 'https://api.aquasensor.co.uk/aq.php?op=readings&username=shu&token=aebbf6305f9fce1d5591ee05a3448eff&sensorid=';
+        $curlOptions = array(
+            CURLOPT_URL => $apiURL . $search,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        );
+        $response = null;
+        $curl = curl_init();
+        // Mats special code
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        //gets api data
+        curl_setopt_array($curl, $curlOptions);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        //gets rid of the \n on the new lines to allow iteration
+        $response = rtrim($response, "\n");
+        $rows = explode("\n", $response);
+
+        //cleans up any extra newlines if the trim didnt work.
+        //https://www.php.net/manual/en/function.array-filter.php
+        $rows = array_filter($rows, function ($row) {
+            return !empty(trim($row));
+        });
+        $rows = array_values($rows);
+
+        //converts to array.
+        $data = array_map('str_getcsv', $rows);
+
+        //removes headers as first array entry
+        array_shift($data);
+        return $data;
     }
 }
