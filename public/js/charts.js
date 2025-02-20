3ef9cc9079db793ef02ee524ad28f15581b89c22
ScottForster400 <scottforster400@gmail.com>
2025-02-20 09:54:55 +0000

    Removed dark mode support

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,4 +1,4 @@
-const tempJson = var sites = {!! json_encode($sites->toArray()) !!};
+// const tempJson = var sites = {!! json_encode($sites->toArray()) !!};
 
 const data = {
     labels: ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'],
