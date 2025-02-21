a3e8672f8b41013063640d935e459562edd6b01d
matkuz162 <kuzonmateusz60@gmail.com>
2025-02-21 13:01:09 +0000

    front end

--- resources/views/layouts/sensor-table.blade.php
+++ resources/views/layouts/sensor-table.blade.php
@@ -84,8 +84,8 @@
                                                             <x-input-error class="mt-2" :messages="$errors->get('longitude')" />
                                                         </div>
                                                         <div class="mb-6">
-                                                            <label for="opensource" class="inline-flex items-center">
-                                                                <select name="opensource" id="opensource" name="opensource">
+                                                            <label for="opensource" class="w-full inline-flex items-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                                                <select name="opensource" id="opensource" name="opensource" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                                                     <option value="1">Open Source</option>
                                                                     <option value="0">Closed Source</option>
                                                                 </select>
