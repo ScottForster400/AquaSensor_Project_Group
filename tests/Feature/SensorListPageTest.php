8a7a857d0d40ea67916becb9da88078c9eb6e81e
matkuz162 <kuzonmateusz60@gmail.com>
2025-03-30 15:19:32 +0100

    sensor list testing

--- tests/Feature/SensorListPageTest.php
+++ tests/Feature/SensorListPageTest.php
@@ -5,75 +5,53 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\Sensor;
+use App\Models\User;
 use Tests\TestCase;
 
 class SensorListPageTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_activate_sensor(): void
+
+
+    public function test_user_can_edit_sensor(): void
     {
 
-        $response = $this->get('/sensors');
+    $user = User::factory()->create(['id' => 1]);
 
-        $sensor = Sensor::factory()->create([
-            'sensor_id' => 'TestingActivation',
-            'activation_key' => '1234',
-        ]);
+    $sensor = Sensor::factory()->create([
+        'sensor_name' => 'sensor_name',
+        'location' => null,
+        'body_of_water' => null,
+        'latitude' => null,
+        'longitude' => null,
+        'activated' => 1,
+        'activation_key' => 'activationkey001',
+        'opensource' => 0,
+    ]);
 
-        $response->assertStatus(200);
+    $response = $this->actingAs($user)->post(route('sensors.update'. $sensor->id), [
+        'sensor_name' => 'updated_name',
+        'location' => 'Hope Valley',
+        'body_of_water' => 'River Derwent',
+        'latitude' => 5,
+        'longitude' => 6,
+    ]);
 
-        $response = $this->post(route('sensors.activate', ['sensor_name' => 'Test2', 'sensor_location' => 'Test2', 'body_of_water' => 'Test2', 'latitude' => '1', 'longitude' => '1', 'activation_key' => '1234', 'opensource' => '1']));
+    $this->assertDatabaseHas('sensors', [
+        'id' => $sensor->id,
+        'sensor_name' => 'updated_name',
+        'location' => 'Hope Valley',
+        'body_of_water' => 'River Derwent',
+        'latitude' => 5,
+        'longitude' => 6,
+        'activated' => 1,
+        'activation_key' => 'activationkey001',
+        'opensource' => 0,
+    ]);
 
-        $this->assertDatabaseHas('sensors', [
-            'sensor_id' => 'TestingActivation',
-            'sensor_name' => 'Test2',
-            'location' => 'Test2',
-            'body_of_water' => 'Test2',
-            'latitude' => '1',
-            'longitude' => '1',
-            'activation_key' => '1234',
-            'opensource' => 1,
-        ]);
+    $response->assertRedirect(route('sensors.index'));
 
-        $response->dump();
-        $response->assertStatus(302);
     }
 
-    public function test_editing_sensors(): void
-    {
-        
-        $response = $this->get('/sensors');
 
-        $sensor = Sensor::factory()->create([
-            'sensor_id' => 'TestingActivation',
-            'sensor_name' => 'Test2',
-            'location' => 'Test2',
-            'body_of_water' => 'Test2',
-            'latitude' => '1',
-            'longitude' => '1',
-            'activation_key' => '1234',
-            'opensource' => 1,
-        ]);
-
-        $response = $this->post(route('sensors.update', ['sensor' => $sensor->sensor_id]), [
-            'sensor_name' => 'Test1',
-            'sensor_location' => 'Test1',
-            'body_of_water' => 'Test1',
-            'latitude' => '2',
-            'longitude' => '2',
-            'activation_key' => '1234',
-            'opensource' => 1,
-        ]);
-
-        $this->assertDatabaseHas('sensors', [
-            'sensor_id' => $sensor->sensor_id,
-            'sensor_name' => 'Test1',
-            'location' => 'Test1',
-            'body_of_water' => 'Test1',
-            'latitude' => '2',
-            'longitude' => '2',
-            'activation_key' => '1234',
-            'opensource' => 1,
-        ]);
-    }
 }
